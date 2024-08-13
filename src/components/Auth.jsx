2b893f8350d8131e8b05b9c8be28924b4d3c34c8
Ranjith <ranjithp2294@gmail.com>
2024-08-13 18:31:23 +0530

    three

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,6 +3,7 @@ import firebase from "../firebase";
 
 const Auth = ()=>{
   const [ phoneNumber,setPhoneNumber] = useState('')
+  const [ verificationCode,setVerificationCode] = useState('')
   const [verificationId,setVerificationId] = useState('')
   const recaptchaRef = useRef(null)
   const handleSendOTP = ()=>{
@@ -18,10 +19,20 @@ const Auth = ()=>{
     .then(confirmationResult=>{
       console.log("confirmationResult",confirmationResult)
       setVerificationId(confirmationResult.verificationId)
-      alert(verificationId)
+      alert("otp send to your mobile")
       //logic
     })
     .catch(error=>console.error('error in sending OTP',error))
+  }
+
+
+  const handleOTP = ()=>{
+    const credentials = firebase.auth.PhoneAuthProvider.credential(verificationId,verificationCode)
+    firebase.auth().signInWithCredential(credentials)
+    .then(userCredentials=>{
+      console.log('user loggend in',userCredentials.user)
+    })
+    .catch(error=>console.log("error in handle otp",error))
   }
   return (
     <>
@@ -32,6 +43,13 @@ const Auth = ()=>{
     onChange={e=>setPhoneNumber(e.target.value)}
     />
     <button onClick={handleSendOTP}>Send OTP</button>
+    <br/>
+    <br/>
+    <input type="text" placeholder="enter otp" 
+    value={verificationCode}
+    onChange={e=>setVerificationCode(e.target.value)}
+    />
+    <button onClick={handleOTP}>enter OTP</button>
     </>
   )
 }
